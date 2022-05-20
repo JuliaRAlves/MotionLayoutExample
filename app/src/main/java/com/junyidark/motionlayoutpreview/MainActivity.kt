299fa5c5891040f52b9ec5b6b986cf5aa0d91c92
Julian Rezende <julia.rezende.18@hotmail.com>
2022-05-20 17:58:31 -0300

    example of artist page with motion layout

---
+++ app/src/main/java/com/junyidark/motionlayoutpreview/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.junyidark.motionlayoutpreview
+
+import android.graphics.Color
+import android.os.Build
+import android.os.Bundle
+import android.view.View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+        setupStatusBar()
+    }
+
+    private fun setupStatusBar() {
+        if (!(Build.VERSION.SDK_INT >= Build.VERSION_CODES.N && isInMultiWindowMode)) {
+            window.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_IN_OVERSCAN);
+            window.statusBarColor = Color.TRANSPARENT
+        }
+
+        val decorView = window.decorView
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+        } else {
+            decorView.systemUiVisibility = decorView.systemUiVisibility or SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        }
+        window.statusBarColor = Color.TRANSPARENT
+    }
+}
---
+++ app/src/main/java/com/junyidark/motionlayoutpreview/MainFragment.kt
@@ -0,0 +1,78 @@
+package com.junyidark.motionlayoutpreview
+
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.palette.graphics.Palette
+import com.google.android.material.appbar.MaterialToolbar
+
+
+class MainFragment : Fragment() {
+
+    private lateinit var toolbar: MaterialToolbar
+    private lateinit var background: AppCompatImageView
+    private lateinit var backgroundColor: AppCompatImageView
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.layout_after, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        toolbar = view.findViewById(R.id.toolbar)
+        background = view.findViewById(R.id.background)
+        backgroundColor = view.findViewById(R.id.background_color)
+
+        setupToolbar()
+
+        val bgResId = getBackgroundResId()
+
+        setRandomBackground(view, bgResId)
+    }
+
+    private fun getBackgroundResId(): Int {
+        val bgList = listOf(R.drawable.bg_blue, R.drawable.bg_black, R.drawable.bg_pink)
+
+        return bgList.shuffled()[0]
+    }
+
+    private fun setRandomBackground(view: View, bgResId: Int) {
+        background.setImageResource(bgResId)
+
+        val dominantColor = getDominantColorByDrawableResId(view, bgResId)
+
+        backgroundColor.background = dominantColor
+    }
+
+    private fun getDominantColorByDrawableResId(view: View, bgResId: Int): ColorDrawable {
+        val bgBitmap = BitmapFactory.decodeResource(view.context.resources, bgResId)
+        val bgColor = Palette.from(bgBitmap).generate().getDominantColor(Color.BLACK)
+
+        return ColorDrawable(bgColor)
+    }
+
+    private fun setupToolbar() {
+        toolbar.let {
+            val navController = findNavController()
+
+            it.setNavigationOnClickListener {
+                if (!navController.navigateUp()) {
+                    requireActivity().finish()
+                }
+            }
+
+            navController.addOnDestinationChangedListener { _, _, _ ->
+                it.setNavigationIcon(R.drawable.ic_nav_back)
+                it.setNavigationIconTint(Color.WHITE)
+            }
+        }
+    }
+}
